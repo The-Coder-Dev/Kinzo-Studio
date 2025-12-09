b408d7c4f082b6e0b2d79af19127cf8824d9591b
Dev Sharma <devsharma2057@gmail.com>
2025-12-09 23:35:43 +0530

    Footer Updated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,7 @@ const links = [
 
 const socialLinks = [
   { title: "Instagram", url: "www.instagram.com", id: 1 },
-  { title: "LinkedIn", url: "https://www.linkedin.com", id: 2 }
+  { title: "LinkedIn", url: "https://www.linkedin.com", id: 2 },
 ];
 
 const Footer = () => {
@@ -16,8 +16,8 @@ const Footer = () => {
     <section className="w-full h-120 bg-primary p-5">
       <div className="bg-orange w-full h-full p-10 flex items-center justify-center rounded-xl ">
         <div className="mx-auto max-w-[1500px] w-full h-full flex flex-col justify-between">
+          {/* Footer Top  */}
           <div className="w-full h-10 flex items-center justify-between">
-            {/* Footer Menu Links */}
             <div className="footer-top-bar">
               <div className="flex gap-6 ">
                 {links.map((item) => {
@@ -44,42 +44,44 @@ const Footer = () => {
                 })}
               </div>
             </div>
+
             {/* Social Links */}
             <div className="flex items-center justify-between">
-                <div className="footer-top-bar">
-              <div className="flex gap-6 ">
-                {socialLinks.map((item) => {
-                  return (
-                    <div
-                      key={item.id}
-                      className="group overflow-hidden relative"
-                    >
-                      <Link
-                        className="font-[inter] font-medium uppercase text-sm block transition-transform duration-300 group-hover:-translate-y-4"
-                        href={item.url}
+              <div className="footer-top-bar">
+                <div className="flex gap-6 ">
+                  {socialLinks.map((item) => {
+                    return (
+                      <div
+                        key={item.id}
+                        className="group overflow-hidden relative"
                       >
-                        {item.title}
-                      </Link>
+                        <Link
+                          className="font-[inter] font-medium uppercase text-sm block transition-transform duration-300 group-hover:-translate-y-4"
+                          href={item.url}
+                        >
+                          {item.title}
+                        </Link>
 
-                      <Link
-                        className="absolute left-0 top-16 block transition-transform duration-300 group-hover:-translate-y-16 font-[inter] font-medium uppercase text-sm "
-                        href={item.url}
-                      >
-                        {item.title}
-                      </Link>
-                    </div>
-                  );
-                })}
+                        <Link
+                          className="absolute left-0 top-16 block transition-transform duration-300 group-hover:-translate-y-16 font-[inter] font-medium uppercase text-sm "
+                          href={item.url}
+                        >
+                          {item.title}
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            </div>
-            
           </div>
 
           <h1 className="text-6xl text-white text-center uppercase">
             Your brand deserves better.<br></br> Let's build it right.
           </h1>
-          <div className="w-full h-10">
+
+          {/* Bottom Footer  */}
+          <div className="w-full h-10 flex items-center justify-between">
             <div className="">
               <Link className="text-3xl italic" href="/">
                 Kinzo
@@ -87,6 +89,47 @@ const Footer = () => {
               <p className="text-sm font-[inter]">
                 2025 © All rights reserved.
               </p>
+            </div>
+
+            <div className="flex items-center justify-between gap-10 font-[inter] font-semibold">
+                {/* Email */}
+              <div className="Email">
+                <h2>Email</h2>
+                <div className="group overflow-hidden relative text-3xl">
+                  <Link
+                    className="font-[inter] font-medium   block transition-transform duration-300 group-hover:-translate-y-16"
+                    href="/"
+                  >
+                    yourmail@gmail.com
+                  </Link>
+
+                  <Link
+                    className="absolute left-0 top-16 block transition-transform duration-300 group-hover:-translate-y-16 font-[inter] font-medium "
+                    href="/"
+                  >
+                    yourmail@gmail.com
+                  </Link>
+                </div>
+              </div>
+                  {/* Phone No */}
+               <div className="">
+                <h2>Phone</h2>
+                <div className="group overflow-hidden relative text-3xl">
+                  <Link
+                    className="font-[inter] font-medium uppercase  block transition-transform duration-300 group-hover:-translate-y-16"
+                    href="/"
+                  >
+                    +91 1234567890
+                  </Link>
+
+                  <Link
+                    className="absolute left-0 top-16 block transition-transform duration-300 group-hover:-translate-y-16 font-[inter] font-medium uppercase "
+                    href="/"
+                  >
+                    +91 1234567890
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
